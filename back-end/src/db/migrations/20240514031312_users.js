e7a87083c5258cb0e54cabc6745efa745e6675e3
Lorraine <dmjlorraine@gmail.com>
2024-05-15 15:15:35 -0500

    updated users migration, seed, and controller; added utils folder; added sample env file

--- back-end/src/db/migrations/20240514031312_users.js
+++ back-end/src/db/migrations/20240514031312_users.js
@@ -2,21 +2,23 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-    return knex.schema.createTable("users", (table) => {
-        table.increments("user_id").primary();
-        table.string("first_name").notNullable();
-        table.string("last_name").notNullable();
-        table.string("username").notNullable().unique();
-        table.string("email").notNullable().unique();
-        table.string("password").notNullable();
-    });
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("user_id").primary();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("username").notNullable().unique();
+    table.string("email").notNullable().unique();
+    table.string("password").notNullable();
+    table.integer("age").notNullable();
+    table.string("occupation").notNullable();
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-    return knex.schema.dropTable("users");
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
 };
