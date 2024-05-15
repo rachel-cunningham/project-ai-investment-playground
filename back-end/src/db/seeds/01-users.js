62b83dc88a8aa0419dc0d44233b8aa84bcc6e569
James-Lycett <jrlycett@mail.com>
2024-05-14 18:19:24 -0700

    added username to users seed file

--- back-end/src/db/seeds/01-users.js
+++ back-end/src/db/seeds/01-users.js
@@ -6,6 +6,6 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('users').del()
   await knex('users').insert([
-    {first_name: "John", last_name: "Smith", email: "johnsmith@email.com", password: "securepassword"},
+    {first_name: "John", last_name: "Smith", username:"jsmith", email: "johnsmith@email.com", password: "securepassword"},
   ]);
 };
