c7e8cd434e47368ae500800733b6d5e6b804963b
Safaa <karamatsafaa0@gmail.com>
2024-05-14 23:29:06 -0600

    new user form

--- front-end/src/pages/SignUpPage.js
+++ front-end/src/pages/SignUpPage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import NewUserForm from "../layouts/users/NewUserForm";
 
 function SignUpPage() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Sign Up Page</h1>
+        <NewUserForm />
         <p>"/dashboard" </p>
         <p>"/log-in"</p>
         <p>"/sign-up</p>
