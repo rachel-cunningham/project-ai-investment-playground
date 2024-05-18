4753c7c4b89efc2c64b100f2e86a2c7d42c7a2d6
James-Lycett <jrlycett@mail.com>
2024-05-18 12:27:27 -0700

    made a little login form to make it easy to login directly to the backend from the browser

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,5 +1,6 @@
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+const bodyParser = require('body-parser');
 
 const cookieParser = require("cookie-parser");
 const express = require("express");
@@ -18,8 +19,12 @@ app.use(
     })
 );
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 app.use("/users", usersRouter);
 app.use("/login", loginRouter);
 
