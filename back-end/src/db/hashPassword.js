68c9f97b52e532c8649641af276aac593e7aaf85
James-Lycett <jrlycett@mail.com>
2024-05-18 11:37:35 -0700

    added users table migration to drop password column and make password_hash notNullable. Updated users seeds so they no longer have a password and they do have password_hash. Saved user credentials for our reference in src/db/seeds/seededUserCredentials.txt. added src/db/hashPassword.js to hash a password (using the right algorithm) and log it so we can add the password in hash form to seed files

---
+++ back-end/src/db/hashPassword.js
@@ -0,0 +1,16 @@
+const bcrypt = require("bcryptjs");
+
+// A little function to hash a password for ya if you wanna update the seeds
+async function hashPassword(password) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const password_hash = await bcrypt.hash(password, salt);
+        console.log(password_hash)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+// Input the password as an arg here, as a string, and save the file
+// run "node hashPassword.js" and copy the log from the console
+hashPassword("password123")
