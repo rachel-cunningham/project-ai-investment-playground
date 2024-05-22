deefccf92b100e81be0a3a5727e96bc1cb1ef846
James Lycett <136135304+James-Lycett@users.noreply.github.com>
2024-05-21 17:37:40 -0700

    Update Cookie Attributes login.controller.js Round 2
    added "partitioned" attribute to res.cookie in login.controller to satisfy brand-spankin-new chrome policy regarding third-party cookies, which is what our cookie becomes (i.e., third-party) the moment it gets served from wealthifyai-backend instead of localhost
    
    hope this works!

--- back-end/src/authentication/login.controller.js
+++ back-end/src/authentication/login.controller.js
@@ -46,6 +46,7 @@ async function login(req, res) {
             secure: process.env.NODE_ENV === "production", // set to process.env.NODE_ENV === 'production' if in production
             sameSite: "none",
             maxAge: 3600000, // 1 hour in milliseconds
+            partitioned
         });
 
         // Sends user info back to frontend
